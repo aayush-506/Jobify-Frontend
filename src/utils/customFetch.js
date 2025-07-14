54d82160bc28f196f65954dc3af93aec89bb7ccb
Aayush Chauhan <at290608@gmail.com>
2025-07-14 18:39:57 +0530

    update customfetch

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: "/api",
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+  withCredentials: true,
 });
 
 export default customFetch;
